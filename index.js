6981411ecb2db1b49b1bd18b619f67add9ae629b
Yogzie <14hodgettd@gmail.com>
2025-04-24 10:55:01 +0100

    Cleaned up clutter

--- index.js
+++ index.js
@@ -1,45 +1,30 @@
 const canvas = document.getElementById("canvasObject");
-const view = canvas.getContext("2d")
+const view = canvas.getContext("2d");
+const particleCount = 2;
 
-const lineRange = 600
-const particleCount = 2
-const radius = 80
-
-
-window.addEventListener("resize", resizeCanvas)
-
+window.addEventListener("resize", resizeCanvas);
 
 function resizeCanvas()
 {
-    canvas.height = innerHeight
-    canvas.width = innerWidth
-    applyLineStyle()
+    canvas.height = innerHeight;
+    canvas.width = innerWidth;
+    setupCanvas();
 };
 
-function applyLineStyle(){
-    view.strokeStyle = "white"
-    view.lineWidth = 2
-    view.lineCap = "round"
-
-    view.fillStyle = "white"
-
+function setupCanvas(){
+    view.strokeStyle = "white";
+    view.lineWidth = 2;
+    view.lineCap = "round";
+    view.fillStyle = "white";
 }
-
-function between(x, min, max){
-    return x >= min && x <= max
-}
-
 
 resizeCanvas()
 
-const array = new Array()
+const particles = [];
 
-
-class Ball {
-    radius = 0
-
-    constructor() {
-        const maxSpeed = 4;
+class Particle {
+    constructor(r = 80) {
+        const maxSpeed = 4
 
         this.x = canvas.width * Math.random()
         this.y = canvas.height * Math.random()
@@ -47,41 +32,33 @@ class Ball {
         this.dx = (maxSpeed * Math.random()) - (maxSpeed / 2)
         this.dy = (maxSpeed * Math.random()) - (maxSpeed / 2)
         
-        this.radius = radius
+        this.radius = r
     }
 
     get diameter() {
-        return this.radius * 2;
+        return this.radius * 2
     }
 
     update() {
 
         if (this.x > canvas.width + this.radius) {
-            this.x -= canvas.width + this.diameter;
+            this.x -= canvas.width + this.diameter
         }
 
         if (this.x < 0 - this.radius) {
-            this.x += canvas.width + this.diameter;
+            this.x += canvas.width + this.diameter
         }
 
         if (this.y > canvas.height + this.radius) {
-            this.y -= canvas.height + this.diameter;
+            this.y -= canvas.height + this.diameter
         }
 
         if (this.y < 0 - this.radius) {
-            this.y += canvas.height + this.diameter;
+            this.y += canvas.height + this.diameter
         }
 
         this.x += this.dx
         this.y += this.dy
-    }
-
-    shrink(arrayRef){
-
-        if (this.radius < 1){
-            this.stopRendering(arrayRef)
-        }
-        this.radius = this.radius - 0.05
     }
 
     draw() {
@@ -93,7 +70,7 @@ class Ball {
     }
 
     stopRendering(arrayRef){
-        array.splice(arrayRef, 1)
+        particles.splice(arrayRef, 1)
     }
 }
 
@@ -101,19 +78,16 @@ class Ball {
 
 
 
-for (let index = 0; index < particleCount; index++) {
-    const b = new Ball()
-    array[index] = b
-}
+for (let index = 0; index < particleCount; index++) 
+    {
+        particles.push(new Particle())
+    }
 
 function animate(){
-
     view.clearRect(0,0,canvas.width, canvas.height)  
-    
-    for (const ball of array) {
-        ball.draw();
+    for (const p of particles) {
+        p.draw();
     }
-
     requestAnimationFrame(animate)
 }
 
