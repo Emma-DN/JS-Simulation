80c82b16d377f3f42f1b1bb97112896087225259
Isaac Turpin <Isaac.turpin2@gmail.com>
2025-05-08 14:36:53 +0100

    practice ship movement

--- index.js
+++ index.js
@@ -4,22 +4,25 @@ const particleCount = 2;
 
 window.addEventListener("resize", resizeCanvas);
 window.addEventListener("keydown", keyDown);
-window.addEventListener("keyup", keyUp)
+window.addEventListener("keyup", keyUp);
 
 let keys = {};
 
-function keyDown(e) {
+function keyDown(e) 
+{
     keys[e.code] = true;
 }
 
-function keyUp(e) {
+function keyUp(e) 
+{
     keys[e.code] = false;
 }
-// if(Keyboard.Left) Left();
-class Keyboard {
-    static get Left() { return !!keys.KeyA }
-    static get Right() { return !!keys.KeyD }
-    static get Thrust() { return !!keys.KeyW }
+
+class Keyboard 
+{
+    static get Left() { return !!keys.KeyA; }
+    static get Right() { return !!keys.KeyD; }
+    static get Thrust() { return !!keys.KeyW; }
 }
 
 function resizeCanvas()
@@ -27,131 +30,180 @@ function resizeCanvas()
     canvas.height = innerHeight;
     canvas.width = innerWidth;
     setupCanvas();
-};
+}
 
-function setupCanvas(){
+function setupCanvas()
+{
     view.strokeStyle = "white";
     view.lineWidth = 2;
     view.lineCap = "round";
     view.fillStyle = "white";
 }
 
-resizeCanvas()
+resizeCanvas();
 
 const particles = [];
 
-class Particle {
-    constructor(r = 80) {
-        const maxSpeed = 4
+class Particle 
+{
+    constructor(r = 80) 
+    {
+        const maxSpeed = 4;
 
-        this.x = canvas.width * Math.random()
-        this.y = canvas.height * Math.random()
+        this.x = canvas.width * Math.random();
+        this.y = canvas.height * Math.random();
 
-        this.dx = (maxSpeed * Math.random()) - (maxSpeed / 2)
-        this.dy = (maxSpeed * Math.random()) - (maxSpeed / 2)
+        this.dx = (maxSpeed * Math.random()) - (maxSpeed / 2);
+        this.dy = (maxSpeed * Math.random()) - (maxSpeed / 2);
         
-        this.radius = r
+        this.radius = r;
     }
 
-    get diameter() {
-        return this.radius * 2
+    get diameter() 
+    {
+        return this.radius * 2;
     }
 
-    update() {
+    update() 
+    {
 
-        if (this.x > canvas.width + this.radius) {
-            this.x -= canvas.width + this.diameter
+        if (this.x > canvas.width + this.radius)
+        {
+            this.x -= canvas.width + this.diameter;
         }
 
-        if (this.x < 0 - this.radius) {
-            this.x += canvas.width + this.diameter
+        if (this.x < 0 - this.radius) 
+        {
+            this.x += canvas.width + this.diameter;
         }
 
-        if (this.y > canvas.height + this.radius) {
-            this.y -= canvas.height + this.diameter
+        if (this.y > canvas.height + this.radius) 
+        {
+            this.y -= canvas.height + this.diameter;
         }
 
-        if (this.y < 0 - this.radius) {
-            this.y += canvas.height + this.diameter
+        if (this.y < 0 - this.radius) 
+        {
+            this.y += canvas.height + this.diameter;
         }
 
-        this.x += this.dx
-        this.y += this.dy
+        this.x += this.dx;
+        this.y += this.dy;
     }
 
-    draw() {
-        this.update()
-
+    draw() 
+    {
+        this.update();
         view.fillRect(this.x, this.y, this.radius, 0, 2 * Math.PI);
     }
 
-    stopRendering(arrayRef){
-        particles.splice(arrayRef, 1)
+    stopRendering(arrayRef)
+    {
+        particles.splice(arrayRef, 1);
     }
 }
 
-class Ship {
+class Ship 
+{
 
-    constructor(xArg = canvas.width / 2, yArg = canvas.height / 2) {
-        this.x = xArg
-        this.y = yArg
-        this.angle = -Math.PI / 2
-        this.radius = 40
+    constructor(xArg = canvas.width / 2, yArg = canvas.height / 2)
+    {
+        this.x = xArg;
+        this.y = yArg;
+        this.angle = -Math.PI / 2;
+        this.radius = 40;
+        this.thrusting = false;
+        this.thrust = {x: 0, y: 0};
+        this.speed = 0.1;
+        this.friction = 0.99;
     }
 
-    draw(){
-        const nose = {
+    draw()
+    {
+
+        const nose = 
+        {
             x: this.radius,
             y: 0
         }
-    if(Keyboard.Left) this.angle -= 0.01;
-    if(Keyboard.Right) this.angle += 0.01;
-        const rotatedPoint = this.rotate(nose)
 
-        const leftPoint = this.rotate(nose, this.angle + Math.PI * .75)
-        const rightPoint = this.rotate(nose, this.angle - Math.PI * .75)
+        // Rotation
+        const rotatedPoint = this.rotate(nose);
+        const leftPoint = this.rotate(nose, this.angle + Math.PI * .75);
+        const rightPoint = this.rotate(nose, this.angle - Math.PI * .75);
 
-        view.beginPath()
-        view.moveTo(this.x + rotatedPoint.x, this.y + rotatedPoint.y)
-        view.lineTo(this.x + leftPoint.x, this.y + leftPoint.y)
-        view.lineTo(this.x, this.y)
+        // Drawing
+        view.beginPath();
+        view.moveTo(this.x + rotatedPoint.x, this.y + rotatedPoint.y);
+        view.lineTo(this.x + leftPoint.x, this.y + leftPoint.y);
+        view.lineTo(this.x, this.y);
+        view.lineTo(this.x + rightPoint.x, this.y + rightPoint.y);
+        view.lineTo(this.x + rotatedPoint.x, this.y + rotatedPoint.y);
+        view.arc(this.x, this.y, this.radius / 2.6, 0, Math.PI * 2);
+        view.fill();
 
-        view.lineTo(this.x + rightPoint.x, this.y + rightPoint.y)
-        view.lineTo(this.x + rotatedPoint.x, this.y + rotatedPoint.y)
-        view.arc(this.x, this.y, this.radius / 2.6, 0, Math.PI * 2)
-
-        view.fill()
+        this.processInput();
+        this.moveShip();
     }
 
-    rotate(point, a = this.angle){
-        const rx = (point.x * Math.cos(a)) - (point.y * Math.sin(a))
-        const ry = (point.x * Math.sin(a)) + (point.y * Math.cos(a))
+    rotate(point, a = this.angle)
+    {
+        const rx = (point.x * Math.cos(a)) - (point.y * Math.sin(a));
+        const ry = (point.x * Math.sin(a)) + (point.y * Math.cos(a));
         return {x: rx, y: ry}
     }
+
+    processInput()
+    {
+        if(Keyboard.Left) this.angle -= 0.05;
+        if(Keyboard.Right) this.angle += 0.05;
+        if(Keyboard.Thrust) 
+        {
+            this.thrusting = true;
+            this.thrust.x += Math.cos(this.angle) * this.speed;
+            this.thrust.y += Math.sin(this.angle) * this.speed;
+        }
+        else
+        {
+            this.thrusting = false;
+        }
+    }
+
+    moveShip()
+    {
+        this.thrust.x *= this.friction;
+        this.thrust.y *= this.friction;
+
+        this.x += this.thrust.x;
+        this.y += this.thrust.y;
+
+        // Screen wrapping
+        if (this.x > canvas.width + this.radius) this.x = -this.radius;
+        if (this.x < -this.radius) this.x = canvas.width + this.radius;
+        if (this.y > canvas.height + this.radius) this.y = -this.radius;
+        if (this.y < -this.radius) this.y = canvas.height + this.radius;
+
+    }
 }
-
-
-
-
 
 const ship = new Ship()
 
 for (let index = 0; index < particleCount; index++) 
-    {
-        particles.push(new Particle())
-    }
+{
+    particles.push(new Particle());
+}
 
-function animate(){
-    view.clearRect(0,0,canvas.width, canvas.height)  
+function animate()
+{
+    view.clearRect(0,0,canvas.width, canvas.height); 
     
-    for (const p of particles) {
+    for (const p of particles) 
+    {
         p.draw();
     }
     ship.draw();
 
-    requestAnimationFrame(animate)
+    requestAnimationFrame(animate);
 }
 
-
-
-animate()
+animate();
